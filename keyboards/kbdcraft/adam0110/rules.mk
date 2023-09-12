1b1419315a18e12be7ab654805f477b21a1f32b2
TC Teo <tsechin@tsechin.com>
2023-09-12 20:17:58 +1000

    move some settings from rules.mk and config.h to info.json

--- keyboards/kbdcraft/adam0110/rules.mk
+++ keyboards/kbdcraft/adam0110/rules.mk
@@ -1,23 +1,2 @@
-# This file intentionally left blank
-# BOARD = BLACKPILL_STM32_F401
-
-# OPT_DEFS += -DBOOTLOADER_TINYUF2
-# BOOTLOADER_TYPE = tinyuf2
-# STM32_BOOTLOADER_ADDRESS = 0x1FFF0000
-LTO_ENABLE = no
-
-# FIRMWARE_FORMAT = uf2 #自加
-
-RGB_MATRIX_ENABLE = yes
-RGB_MATRIX_DRIVER = ws2812
-# RGBLIGHT_ENABLE = yes
-# RGBLIGHT_DRIVER = WS2812
-WS2812_DRIVER = pwm
-
-SPLIT_KEYBOARD = yes
 SERIAL_DRIVER = usart
-
-OPT_DEFS += -DCORTEX_ENABLE_WFI_IDLE=TRUE
-
-RGB_MATRIX_CUSTOM_USER = yes # includes rgb_matrix_user.inc
-CONSOLE_ENABLE = yes
+RGB_MATRIX_CUSTOM_USER = yes # rgb_matrix custom animations from rgb_matrix_user.inc
