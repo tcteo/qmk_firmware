36d625ade8410b6134a896feb38d80664b10ea50
TC Teo <tsechin@tsechin.com>
2023-09-12 22:12:04 +1000

    custom keymap update/cleanup

--- keyboards/kbdcraft/adam0110/keymaps/tcteo/keymap.c
+++ keyboards/kbdcraft/adam0110/keymaps/tcteo/keymap.c
@@ -3,40 +3,47 @@
 
 enum layers {
     _BASE = 0,
-    _BASE_AAPL,
+    _AAPL,
     _FN1,
-    _FN2
+    _FN2,
+    _FN3
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE] = LAYOUT_64_ansi(
-        KC_GRV,  KC_1,    KC_2,    KC_3,   KC_4,    KC_5,     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,  KC_EQL,  KC_BSPC,
-        KC_TAB,  KC_Q,    KC_W,    KC_E,   KC_R,    KC_T,     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,  KC_RBRC, KC_BSLS,
-        KC_ESC,  KC_A,    KC_S,    KC_D,   KC_F,    KC_G,     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,           KC_ENT,
-        KC_LSFT, KC_Z,    KC_X,    KC_C,   KC_V,    KC_B,     KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,  KC_UP,   KC_NO,
-        KC_LCTL, KC_LGUI, KC_LALT,                  KC_SPC,                              KC_NO,   MO(_FN1),KC_LEFT,  KC_DOWN, KC_RGHT
+        KC_GRV,   KC_1,    KC_2,    KC_3,   KC_4,    KC_5,     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,     KC_MINS, KC_EQL,  KC_BSPC,
+        KC_TAB,   KC_Q,    KC_W,    KC_E,   KC_R,    KC_T,     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,     KC_LBRC, KC_RBRC, KC_BSLS,
+        KC_ESC,   KC_A,    KC_S,    KC_D,   KC_F,    KC_G,     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,  KC_QUOT,          KC_ENT,
+        KC_LSFT,  KC_Z,    KC_X,    KC_C,   KC_V,    KC_B,     KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT, KC_UP,   KC_NO,
+        KC_LCTL,  KC_LGUI, KC_LALT,                  KC_SPC,                              MO(_FN2),MO(_FN1), KC_LEFT, KC_DOWN, KC_RGHT
  ),
-    [_BASE_AAPL] = LAYOUT_64_ansi(
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_TRNS, KC_LOPT, KC_LCMD,                   KC_TRNS,                            KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+    [_AAPL] = LAYOUT_64_ansi(
+        KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,          KC_TRNS,
+        KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS,  KC_LOPT, KC_LCMD,                   KC_TRNS,                            KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS
     ),
     [_FN1] = LAYOUT_64_ansi(
-        KC_NO,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,   KC_F11,   KC_F12,   KC_DEL,
-        KC_NO,   RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, RGB_MOD, KC_PSCR,  KC_NO,    KC_NO,    KC_NO,
-        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   RGB_SPI, RGB_SPD, KC_NO,    KC_VOLU,            KC_NO,
-        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   NK_TOGG, KC_NO,   KC_NO,   KC_INS,  KC_NO,    KC_VOLD,  KC_PGUP,  KC_MUTE,
-        KC_NO,   KC_NO,   KC_NO,                     KC_NO,                              MO(_FN2),KC_TRNS,  KC_HOME,  KC_PGDN,  KC_END
+        KC_NO,    KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,   KC_F11,  KC_F12,  KC_DEL,
+        KC_NO,    KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_PSCR,  KC_NO,   KC_NO,   KC_NO,
+        KC_NO,    KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,    KC_VOLU,          KC_NO,
+        KC_NO,    KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   NK_TOGG, KC_NO,   KC_NO,   KC_INS,  KC_NO,    KC_VOLD, KC_PGUP, KC_MUTE,
+        KC_NO,    KC_NO,   KC_NO,                     KC_NO,                              MO(_FN3),KC_NO,    KC_HOME, KC_PGDN, KC_END
     ),
-
     [_FN2] = LAYOUT_64_ansi(
-        TG(_BASE_AAPL), KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   EE_CLR,
-        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   QK_RBT,
-        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,            KC_NO,
-        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
-        KC_NO,   KC_NO,   KC_NO,                     KC_NO,                              KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO
+        KC_NO,    KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,    KC_NO,   KC_NO,   KC_NO,   KC_NO,
+        KC_NO,    KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,    KC_NO,   KC_NO,   KC_NO,   KC_NO,
+        KC_NO,    KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,    KC_NO,   KC_NO,            KC_NO,
+        KC_NO,    KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,    KC_NO,   KC_NO,   KC_NO,   KC_NO,
+        KC_NO,    KC_NO,   KC_NO,                     KC_NO,                              KC_NO,    MO(_FN3),KC_NO,   KC_NO,   KC_NO
+    ),
+    [_FN3] = LAYOUT_64_ansi(
+        TG(_AAPL),KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,    KC_NO,   KC_NO,   KC_NO,   EE_CLR,
+        RGB_TOG,  RGB_HUI, RGB_SAI, RGB_VAI, RGB_SPI, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,    KC_NO,   KC_NO,   KC_NO,   QK_RBT,
+        RGB_MOD,  RGB_HUD, RGB_SAD, RGB_VAI, RGB_SPD, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,    KC_NO,   KC_NO,            KC_NO,
+        KC_NO,    KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,    KC_NO,   KC_NO,   KC_NO,   KC_NO,
+        KC_NO,    KC_NO,   KC_NO,                     KC_NO,                              KC_NO,    KC_NO,   KC_NO,   KC_NO,   KC_NO
     ),
 };
 
@@ -50,14 +57,17 @@ layer_state_t layer_state_set_user(layer_state_t state) {
         case _BASE:
             xprintf("_BASE\n");
             break;
-        case _BASE_AAPL:
-            xprintf("layer: _BASE_AAPL\n");
+        case _AAPL:
+            xprintf("layer: _AAPL\n");
             break;
         case _FN1:
             xprintf("layer: _FN1\n");
             break;
         case _FN2:
             xprintf("layer: _FN2\n");
+            break;
+        case _FN3:
+            xprintf("layer: _FN3\n");
             break;
         default:
             xprintf("layer: ???\n");
@@ -71,7 +81,7 @@ layer_state_t layer_state_set_user(layer_state_t state) {
         case _BASE:
             rgb_matrix_set_color(led_num, 0x00, 0x00, 0xFF); // blue
             break;
-        case _BASE_AAPL:
+        case _AAPL:
             rgb_matrix_set_color(led_num, 0xFF, 0x00, 0x00); // red
             break;
         case _FN1:
@@ -79,6 +89,9 @@ layer_state_t layer_state_set_user(layer_state_t state) {
             break;
         case _FN2:
             rgb_matrix_set_color(led_num, 0xFF, 0x00, 0xFF); // magenta
+            break;
+        case _FN3:
+            rgb_matrix_set_color(led_num, 0xFF, 0xFF, 0x00); // yellow
             break;
         default:
             rgb_matrix_set_color(led_num, 0xFF, 0xFF, 0xFF); // white
